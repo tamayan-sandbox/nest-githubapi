9a69d833f3331611224b5b4e6517de8332a42624
Yuki Kitagawa <tamayan.db18+dev@gmail.com>
2022-04-20 19:25:13 +0900

    env を扱う

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GithubModule } from './github/github.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [GithubModule],
+  imports: [
+    GithubModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
