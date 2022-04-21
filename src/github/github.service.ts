83744893d0c267912b7b07edededc12777fe3185
Yuki Kitagawa <tamayan.db18+dev@gmail.com>
2022-04-21 14:57:58 +0900

    リファクタ

--- src/github/github.service.ts
+++ src/github/github.service.ts
@@ -1,30 +1,32 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { firstValueFrom } from 'rxjs';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class GithubService {
   constructor(private httpService: HttpService) {}
   async getUser(name: string): Promise<string> {
     const url = 'https://api.github.com/users/' + name;
-    const res = await firstValueFrom(this.httpService.get(url));
+    const res = await lastValueFrom(this.httpService.get(url));
     return res.data;
   }
 
   async getIssues(owner: string, repo: string): Promise<string> {
-    const url = 'https://api.github.com/repos/' + owner + '/' + repo + '/issues';
-    const res = await firstValueFrom(this.httpService.get(url));
+    const url =
+      'https://api.github.com/repos/' + owner + '/' + repo + '/issues';
+    const res = await lastValueFrom(this.httpService.get(url));
     return res.data;
   }
 
   async createIssues(owner: string, repo: string, data): Promise<string> {
-    const url = 'https://api.github.com/repos/' + owner + '/' + repo + '/issues';
+    const url =
+      'https://api.github.com/repos/' + owner + '/' + repo + '/issues';
     const headers = {
       'Content-Type': 'application/json',
       Accept: 'application/vnd.github.v3+json',
       Authorization: 'token ' + process.env.GITHUB_TOKEN,
     };
-    const res = await firstValueFrom(
+    const res = await lastValueFrom(
       this.httpService.post(url, data, { headers: headers }),
     );
     return res.data;
